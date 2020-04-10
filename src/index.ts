876eb3a8704735b3072e6664bf7ba562efcd3d7a
jarvan <532547613@qq.com>
2020-04-10 19:22:52 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function hello(compiler: string) {
   console.log(`Hello from ${compiler}`);
   let s = 122;
   const ss = 233;
-  let sddd = 211;
+  let sddd = 211333;
   console.log(s + 1);
 }
 hello("TypeScript");
