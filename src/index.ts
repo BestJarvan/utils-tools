9767a9643d7108d953dd6d583bdeeff593df311a
jarvan <532547613@qq.com>
2020-04-10 19:28:16 +0800

    udpate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-function hello(compiler: string) {
-  console.log(`Hello from ${compiler}`);
-  let s = 122;
-  const ss = 233;
-  let sddd = 211333;
-  console.log(s + 1);
+function hello (compiler: string) {
+  console.log(`Hello from ${compiler}`)
+  let s = 122
+  const ss = 233
+  let sddd = 2122221333
+  console.log(s + 1)
 }
-hello("TypeScript");
+hello('TypeScript')
