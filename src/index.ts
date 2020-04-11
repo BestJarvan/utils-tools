b54f54c8f5f5417bdb25618d5cd8d316c6c67a54
jarvan <532547613@qq.com>
2020-04-11 22:09:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-function hello(compiler: string) {
-  console.log(`Hello from ${compiler}`)
+/**
+ * 判断是否为对象类型, 这里 和 isPlainObject 的区别是 isObject 为数组、FormData 都会返回 true
+ * @param value 需要检测的值
+ * @return 返回 true 或者 false
+ * @author jarvan
+ * ```typescript
+ *
+ * const flag = inyUtils.isObject({})
+ * // flag === true
+ * ```
+ */
+interface Us {
+  name: string
+  age?: string | number
+}
+function hello(obj: Us): void {
+  console.log(`Hello from ${obj.name}, ${obj.age}asdsad`)
   const s = 1223213232132
   console.log(s + 1)
 }
-hello('TypeScript')
+let obj = { name: 'sss', age: 2 }
+hello(obj)
