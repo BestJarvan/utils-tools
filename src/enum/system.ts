4a04fff20908983df158b010ff70b1df46fe1bea
陈子豪 <zihao.chen@xbongbong.com>
2021-09-10 14:51:52 +0800

    fix: 加入注释

--- src/enum/system.ts
+++ src/enum/system.ts
@@ -2,8 +2,12 @@
  * @Author: zihao.chen
  * @Date: 2021-09-10 10:16:49
  * @LastEditors: zihao.chen
- * @LastEditTime: 2021-09-10 10:21:10
+ * @LastEditTime: 2021-09-10 14:51:43
  * @Description:
+ */
+/**
+ * @ignore
+ * @description 多平台枚举
  */
 export enum System {
   'wx' = 'wxwork',
