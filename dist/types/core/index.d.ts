eafc16a985a83c4a1cd5dca1c94a04127c10484d
‘蒋亚辉’ <532547613@qq.com>
2021-09-18 16:38:24 +0800

    feat: release 1.2.3

--- dist/types/core/index.d.ts
+++ dist/types/core/index.d.ts
@@ -2,75 +2,75 @@
  * @ignore
  * @description 解析JSON Object，并加入异常处理
  */
-export declare function json(str: string, def?: any): any;
+export declare function json(str: string, def?: any): any
 /**
  * @ignore
  * @description 检测是否是钉钉环境
  */
-export declare function isDingTalk(): boolean;
+export declare function isDingTalk(): boolean
 /**
  * @ignore
  * @description 是否为开发环境
  */
-export declare function isDevEnv(): boolean;
+export declare function isDevEnv(): boolean
 /**
  * @ignore
  * @description 计算字符长度：汉字为1，英文字母为0.5，结果取整
  */
-export declare function getByteLen(val: string | undefined | null): number;
+export declare function getByteLen(val: string | undefined | null): number
 /**
  * @ignore
  * @description 深拷贝
  */
-export declare function deepClone(source: any, nullVal?: null | undefined): any;
+export declare function deepClone(source: any, nullVal?: null | undefined): any
 /**
  * @ignore
  * @description json 返回指定列
  */
-export declare function getParamValues(name: number, arr: Array<any[]>): Array<any>;
+export declare function getParamValues(name: number, arr: Array<any[]>): Array<any>
 /**
  * @ignore
  * @description 1和0互相转换，取反函数
  */
-export declare function reverseNum(num: number | string): number | void;
+export declare function reverseNum(num: number | string): number | void
 /**
  * @ignore
  * @description 检测对象类型
  */
-export declare function getObjectType(obj: any): string;
+export declare function getObjectType(obj: any): string
 /**
  * @ignore
  * @description 对象数组去重
  */
-export declare function noRepeatArray(repeatArray: Array<any>, attr: string): any[];
+export declare function noRepeatArray(repeatArray: Array<any>, attr: string): any[]
 /**
  * @ignore
  * @description 判断当前是否在外链表单页面中
  */
-export declare function isOuterLinkEnv(): boolean;
+export declare function isOuterLinkEnv(): boolean
 /**
  *
  * @ignore
  * @description 获取浏览器信息
  */
-export declare function browser(): string;
+export declare function browser(): string
 /**
  * @ignore
  * @description 是否企微图片
  */
-export declare function isWxImg(img: string): string;
+export declare function isWxImg(img: string): string
 /**
  * @ignore
  * @description 获取图片地址
  */
-export declare function thumbnail(img: string | string[], size?: number): string;
+export declare function thumbnail(img: string | string[], size?: number, oss?: number): string
 /**
  * @ignore
  * @description 移动端判断是否当前多平台环境
  */
-export declare function envInfo(name: string[]): boolean;
+export declare function envInfo(name: string[]): boolean
 /**
  * @ignore
  * @description 判断是否第三方pc端
  */
-export declare function isThirdPC(name: string[], inBrowser: boolean): boolean;
+export declare function isThirdPC(name: string[], inBrowser: boolean): boolean
