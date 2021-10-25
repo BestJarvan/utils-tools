b4c0f3c8789a310fbc6bfe550ea6771c00e8ae02
蒋亚辉 <j532547613@gmail.com>
2021-10-25 10:40:18 +0800

    feat: release v1.3.4

--- dist/types/core/method.d.ts
+++ dist/types/core/method.d.ts
@@ -4,11 +4,25 @@
  */
 /**
  * @ignore
- * 节流函数
+ * 防抖函数
+ * 默认调用在防抖开始后
  */
-export declare function throttle(fn: Function, delay?: number): Function;
+export declare function debounce(fn: any, delay?: number, option?: {}): import("lodash").DebouncedFunc<any>;
 /**
  * @ignore
- * 防抖函数
+ * 防抖装饰器
+ * 默认调用在防抖开始后
  */
-export declare function debounce(fn: Function, delay?: number): Function;
+export declare function debounceWrap(delay?: number, option?: {}): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
+/**
+ * @ignore
+ * 节流函数
+ * 默认调用在节流开始前
+ */
+export declare function throttle(fn: any, delay?: number, option?: {}): import("lodash").DebouncedFunc<any>;
+/**
+ * @ignore
+ * 节流装饰器
+ * 默认调用在节流开始前
+ */
+export declare function throttleWrap(delay?: number, option?: {}): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
